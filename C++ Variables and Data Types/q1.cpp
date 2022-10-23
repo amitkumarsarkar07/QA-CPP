cd59254a92bce92d4853a3a80ee7d22c37518002
AMIT KUMAR SARKAR <115895407+amitkumarsarkar07@users.noreply.github.com>
2022-10-23 21:26:54 +0530

    Create q1.cpp

---
+++ C++ Variables and Data Types/q1.cpp
@@ -0,0 +1,14 @@
+//Take two integer values in two variabales x and y and print their product.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int x,y;
+cout<<"Enter tha 1st value"<<endl;
+cin>>x;
+cout<<"Enter tha 2st value"<<endl;
+cin>>y;
+cout<<"Tha product is: "<<x*y;
+return 0;
+}
